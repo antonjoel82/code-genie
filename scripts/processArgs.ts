b683a7a9782db615531797f94ec184c8bffe698e
Joel Anton <j.anton.dev@gmail.com>
2022-12-26 16:30:03 -0800

    Update arg processing

--- scripts/processArgs.ts
+++ scripts/processArgs.ts
@@ -1,8 +1,10 @@
-import minimist from "minimist";
+import minimist, { Opts } from "minimist";
 
 const FIRST_PARAM_INDEX: number = 2;
 
-const ALIASES: Record<string, string | string[]> = {
+type AliasDictionary = Opts["alias"];
+
+export const DEFAULT_ALIASES: AliasDictionary = {
   path: ["p"],
   lang: ["l", "language"],
   spec: ["specialization"],
@@ -10,10 +12,18 @@ const ALIASES: Record<string, string | string[]> = {
 };
 
 const ALLOWED_UNLABELED_ARG = "name";
-const ALLOWED_UNLABELED_ARGS = [
-  ALLOWED_UNLABELED_ARG,
-  ...ALIASES[ALLOWED_UNLABELED_ARG],
-];
+
+export interface ProcessArgsOptions {
+  /** Alternative names / labels for a given command-line argument */
+  aliases?: AliasDictionary;
+  /** Command-line argument name that is permitted to be unnamed / unlabeled */
+  allowedUnlabeledArg?: string | null;
+}
+
+const DEFAULT_PROCESS_ARGS_OPTIONS: ProcessArgsOptions = {
+  aliases: DEFAULT_ALIASES,
+  allowedUnlabeledArg: ALLOWED_UNLABELED_ARG,
+};
 
 export interface ProcessedArgs {
   primary: string;
@@ -29,27 +39,36 @@ export interface ProcessedArgs {
  */
 export const processArgs = (
   argv: string[],
-  options: {} = {}
+  {
+    aliases,
+    allowedUnlabeledArg,
+  }: ProcessArgsOptions = DEFAULT_PROCESS_ARGS_OPTIONS
 ): ProcessedArgs => {
+  // setup
+  const aggregatedAllowedUnlabeledArgs: string[] = !allowedUnlabeledArg
+    ? []
+    : [allowedUnlabeledArg, ...aliases[allowedUnlabeledArg]];
+
+  console.log({ aliases });
   const parsedArgs = minimist(argv.slice(FIRST_PARAM_INDEX), {
-    alias: ALIASES,
+    alias: aliases,
   });
 
   // if there aren't any unnamed flags
-  const model: string | undefined = ALLOWED_UNLABELED_ARGS.reduce(
+  const model: string | undefined = aggregatedAllowedUnlabeledArgs.reduce(
     (outputVal, aliasName) => outputVal || parsedArgs[aliasName],
     parsedArgs._[0]
   );
 
-  if (!model) {
-    throw new Error(
-      "Error! No primary name provided for generating your file(s)."
-    );
-  }
+  // if (!model) {
+  //   throw new Error(
+  //     "Error! No primary name provided for generating your file(s)."
+  //   );
+  // }
 
-  if (parsedArgs._.length > 1) {
-    throw new Error("Error! Too many unlabeled arguments provided.");
-  }
+  // if (parsedArgs._.length > 1) {
+  //   throw new Error("Error! Too many unlabeled arguments provided.");
+  // }
 
   return {
     primary: model,
